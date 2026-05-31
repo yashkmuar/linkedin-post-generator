9c4f4b3e2ceacbb8ebd055cafb2b22f791ef7bbe
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 21:50:47 +0530

    FormsModule Integrated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
