44feedfb2a7e50f7d02998930e196abea10e69f8
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 13:21:41 +0530

    Connecting router to the main api engine file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.post_routes import router as post_router
+
+app = FastAPI(
+    title="LinkedIn Post Generator API"
+)
+
+app.include_router(
+    post_router,
+    prefix="/api/posts",
+    tags=["Posts"]
+)
 
 @app.get("/")
 def home():
