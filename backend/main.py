95e03f3837b92b651fcad4689a25d92246a9de3c
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 22:10:15 +0530

    due to angular flight request couldn't understand by FASTAPI I make sure that it is validated through middleware pipeline where for the port 4200 in the localhost, I make exception

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.post_routes import router as post_router
 
 app = FastAPI(
     title="LinkedIn Post Generator API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
