f6f8e38fb8adf0ee3c5c7904f8b4f97cfe628ed1
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 16:29:15 +0530

    Tone enum created

--- backend/app/models/post_models.py
+++ backend/app/models/post_models.py
@@ -1,4 +1,12 @@
+from enum import Enum
 from pydantic import BaseModel
+
+class ToneEnum(str, Enum):
+    PROFESSIONAL = "Professional"
+    GEN_Z="Gen-Z"
+    FOUNDER="Founder"
+    CORPORATE="Corporate"
+    MOTIVATIONAL="Motivational" 
 
 class PostGeneratorRequest(BaseModel):
     topic: str
