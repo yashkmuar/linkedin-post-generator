27462f2c6e78a5d1d6395c4371ad8f30fe105675
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 13:32:10 +0530

    now the the POST API request is validated by the model first and the respective response is generated in the predictable format

--- backend/app/api/post_routes.py
+++ backend/app/api/post_routes.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 
+from app.models.post_models import (
+    PostGeneratorRequest,
+    PostGeneratorResponse
+)
+
 router = APIRouter()
 
 @router.get("/health")
@@ -8,3 +13,15 @@ def health_check():
         "status":"success",
         "message":"Post API is working!!"
     }
+
+
+@router.post(
+    "/generate",
+    response_model=PostGeneratorResponse
+)
+def generate_post(
+    request: PostGeneratorRequest
+):
+    return PostGeneratorResponse(
+        post = f"LinkedIn post about: {request.topic}"
+    )
