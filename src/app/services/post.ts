e2ddb5fd2fb5db988db24973aae27b82f81057a7
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 20:23:48 +0530

    now the request is pre-validated means users should provide topic, tone, audience, length, before sending request to our model

--- src/app/services/post.ts
+++ src/app/services/post.ts
@@ -1,5 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { GeneratePostRequest, GeneratePostResponse } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +11,8 @@ export class Post {
 
   private http = inject(HttpClient);
   
-  generatePost(payload: any){
-    return this.http.post(
+  generatePost(payload: GeneratePostRequest): Observable<GeneratePostResponse>{
+    return this.http.post<GeneratePostResponse>(
       'http://127.0.0.1:8000/api/posts/generate',
       payload
     );
