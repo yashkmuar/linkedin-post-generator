22705dee74060ceba765c4d76c2d3c8fe17b48e2
Yash Kumar <yashkumarpal1988@gmail.com>
2026-05-31 16:05:30 +0530

    Now , the system prompt topic is embbeded from the prompt send by the user and based on that overall prompt the Linkedin post generated

--- backend/app/services/post_service.py
+++ backend/app/services/post_service.py
@@ -1,8 +1,29 @@
+from langchain_openai import AzureChatOpenAI
+from app.core.config import settings
+from app.prompts.linkedin_prompt import (
+    LINKEDIN_POST_PROMPT
+)
+
+
 class PostService:
+
+    def __init__(self):
+        self.llm = AzureChatOpenAI(
+            azure_endpoint = settings.AZURE_OPENAI_ENDPOINT,
+            api_key = settings.AZURE_OPENAI_API_KEY,
+            deployment_name = settings.AZURE_OPENAI_DEPLOYMENT,
+            api_version = "2024-02-01"
+        )
 
     def generate_post(
             self,
-            topic:str
+            topic: str
     ) -> str:
         
-        return f"LinkedIn post about: {topic}"
+        prompt = LINKEDIN_POST_PROMPT.format(topic = topic)
+
+        response = self.llm.invoke(
+            prompt
+        )
+
+        return response.content
