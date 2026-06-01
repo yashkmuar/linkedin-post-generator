a2182d77b13f799ad5a4a9f7e43c70886b37aa0f
Yash Kumar <yashkumarpal1988@gmail.com>
2026-06-01 15:15:43 +0530

    Now rather than calling the post api from app, we have a separate component for it

--- src/app/components/post-generator/post-generator.ts
+++ src/app/components/post-generator/post-generator.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Post } from '../../services/post';
 
 @Component({
   selector: 'app-post-generator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-generator.html',
   styleUrl: './post-generator.css',
 })
 export class PostGenerator {
+
+  private postService = inject(Post);
+
+  topic = '';
+  audience = '';
+
+  tone = 'Professional';
+  length = 'Medium';
+
+  generatedPost = '';
+
+  generatePost() {
+
+    this.postService.generatePost({
+      topic: this.topic,
+      tone: this.tone,
+      audience: this.audience,
+      length: this.length
+    }).subscribe(response => {
+      this.generatedPost = response.post;
+    });
+  }
 
 }
