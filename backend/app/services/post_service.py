235a105103d4540c3b1a57d4c7279e0c178a5fa2
--global <--global>
2026-06-29 14:42:03 +0530

    applied chains on editor , writer and strategy prompts

--- backend/app/services/post_service.py
+++ backend/app/services/post_service.py
@@ -1,7 +1,13 @@
 from langchain_openai import AzureChatOpenAI
 from app.core.config import settings
+from langchain_core.output_parsers import StrOutputParser
+from openai import BadRequestError
+from fastapi import HTTPException
+
 from app.prompts.linkedin_prompt import (
-    LINKEDIN_POST_PROMPT
+    STRATEGY_PROMPT,
+    WRITER_PROMPT,
+    EDITOR_PROMPT
 )
 
 
@@ -15,6 +21,12 @@ class PostService:
             api_version = "2024-02-01"
         )
 
+        self.parser = StrOutputParser()
+
+        self.strategy_chain = STRATEGY_PROMPT | self.llm | self.parser
+        self.writer_chain = WRITER_PROMPT | self.llm | self.parser
+        self.editor_chain = EDITOR_PROMPT | self.llm | self.parser
+
     def generate_post(
             self,
             topic: str,
@@ -23,14 +35,50 @@ class PostService:
             length: str
     ) -> str:
         
-        prompt = LINKEDIN_POST_PROMPT.format(
-            topic = topic,
-            tone = tone,
-            audience = audience,
-            length = length)
+        try:
+            tone_value = getattr(tone, "value", tone)
+            length_value = getattr(length, "value", length)
 
-        response = self.llm.invoke(
-            prompt
-        )
+            strategy = self.strategy_chain.invoke({
+                "topic": topic,
+                "tone": tone_value,
+                "audience": audience,
+                "length": length_value
+            })
 
-        return response.content
+            draft = self.writer_chain.invoke({
+                "topic": topic,
+                "tone": tone_value,
+                "audience": audience,
+                "length":   length_value,
+                "strategy": strategy
+            })
+
+            final_post = self.editor_chain.invoke({
+                "draft":draft
+            })
+
+            return final_post.strip()
+        
+        except BadRequestError as ex:
+            error_text = str(ex)
+
+            if "content_filter" in error_text or "ResponsibleAIPolicyViolation" in error_text:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Azure OpenAI blocked this request due to content safety filtering. Please try a more professional Linkedin topic."
+                )
+            
+            raise HTTPException(
+                status_code = 400,
+                detail="Azure OpenAI rejected the request. Please check your input."
+            )
+        
+        except Exception as ex:
+            print(ex)
+            raise HTTPException(
+                status_code=500,
+                detail="Unable to generate Linkedin post."
+            )
+        
+        
