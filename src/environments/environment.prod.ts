81770b7917f8c4c60b555cc4a4b09de38a6ed42c
--global <--global>
2026-06-30 22:53:42 +0530

    added backend container apps url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://api.yaslinkedpostgenerator.com/api/posts'
+    apiBaseUrl: 'https://ca-linkedin-post-backend.livelydesert-c52fc7ba.eastus2.azurecontainerapps.io'
 };
