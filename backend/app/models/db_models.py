387bb3c43f1c404fc30a5a744bbe22925db9a67e
--global <--replace-all>
2026-07-22 10:14:55 +0530

    added userpreference and postgenerations tables classes objects

--- backend/app/models/db_models.py
+++ backend/app/models/db_models.py
@@ -25,4 +25,72 @@ class UserSession(Base):
     last_seen_at: Mapped[datetime] = mapped_column(
         TIMESTAMP
     )
-    
+
+class PostGeneration(Base):
+
+    __tablename__ = "post_generations"
+
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True
+    )
+
+    session_id: Mapped[UUID] = mapped_column(
+        ForeignKey("user_sessions.id")
+    )
+
+    topic: Mapped[str] = mapped_column(
+        Text
+    )
+
+    audience: Mapped[str] = mapped_column(
+        Text
+    )
+
+    tone: Mapped[str] = mapped_column(
+        VARCHAR(50)
+    )
+
+    length: Mapped[str] = mapped_column(
+        VARCHAR(20)
+    )
+
+    generated_post: Mapped[str] = mapped_column(
+        Text
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP
+    )
+
+
+class UserPreference(Base):
+    __tablename__ = "user_preferences"
+
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True
+    )
+
+    session_id: Mapped[UUID] = mapped_column(
+        ForeignKey("user_sessions.id"),
+        unique=True
+    )
+
+    preferred_tone: Mapped[str | None] = mapped_column(
+        VARCHAR(50)
+    )
+
+    preferred_length: Mapped[str | None] = mapped_column(
+        VARCHAR(20)
+    )
+
+    preferred_audience: Mapped[str | None] = mapped_column(
+        Text
+    )
+
+    style_summary: Mapped[str | None] = mapped_column(
+        Text
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP
+    )
