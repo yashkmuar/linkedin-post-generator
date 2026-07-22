c4c7dc3f5fa5b29c1b9176cf8e939b17a47e5673
--global <--replace-all>
2026-07-22 12:32:29 +0530

    adding save_generations method, so that user gneerated post save in the database

--- backend/app/services/history_service.py
+++ backend/app/services/history_service.py
@@ -1,5 +1,8 @@
 from uuid import UUID
 
+from datetime import datetime
+from uuid import uuid4
+
 from sqlalchemy import select
 
 from app.core.database import SessionLocal
@@ -25,3 +28,33 @@ class HistoryService:
             )
 
             return result.scalars().all()
+        
+
+    async def save_generation(
+            self,
+            session_id,
+            topic,
+            audience,
+            tone,
+            length,
+            generated_post
+    ):
+        
+        async with SessionLocal() as session:
+
+            post = PostGeneration(
+                id=uuid4(),
+                session_id=session_id,
+                topic=topic,
+                audience=audience,
+                tone=tone,
+                length=length,
+                generated_post=generated_post,
+                created_at=datetime.now()
+            )
+
+            session.add(post)
+
+            await session.commit()
+
+            return post
